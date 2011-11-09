62d686d5f7fdc73a1bf05e07bb12e3a5ff057aed
Axo <axelforest@gmail.com>
2011-11-09 12:16:44 -0500

    Tutorial 3 - continuing the Web-poll Application. Creating the public interface "views"

--- polls/views.py
+++ polls/views.py
@@ -1,1 +1,40 @@
 # Create your views here.
+#from django.template import Context, loader
+#from django.http import HttpResponse
+#from django.http import Http404
+from django.shortcuts import render_to_response, get_object_or_404
+from django.shortcuts import render_to_response
+from polls.models import Poll
+
+
+def index(request):
+	latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
+	# Old code
+	#t = loader.get_template('polls/index.html')
+	#c = Context({
+	#	'latest_poll_list': latest_poll_list,
+	#})
+	#return HttpResponse(t.render(c))
+	# Same thing rewritten
+	return render_to_response('polls/index.html', {'latest_poll_list': latest_poll_list})
+
+def detail(request, poll_id):
+	#return HttpResponse("You're looking at poll %s." % poll_id)
+	# Old code
+	#try:
+	#	p = Poll.objects.get(pk=poll_id)
+	#except Poll.DoesNotExist:
+	#	raise Http404
+	#return render_to_response('polls/detail.html', {'poll': p})
+	# Samed thin rewritten
+	p = get_object_or_404(Poll, pk=poll_id)
+	return render_to_response('polls/detail.html', {'poll': p})
+
+def results(request, poll_id):
+	#return HttpResponse("You're looking at the results of poll %s." % poll_id)
+	return render_to_response('polls/results.html', {'poll_id': poll_id})
+
+def vote(request, poll_id):
+	#return HttpResponse("You're voting on poll %s." % poll_id)
+	return render_to_response('polls/vote.html', {'poll_id': poll_id})
+
--- urls.py
+++ urls.py
@@ -8,6 +8,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'DjangoApp.views.home', name='home'),
     # url(r'^DjangoApp/', include('DjangoApp.foo.urls')),
+    url(r'^polls/', include('polls.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
@@ -15,3 +16,5 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 )
+
+
